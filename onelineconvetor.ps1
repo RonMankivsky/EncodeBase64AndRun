b860565890a2a3a67da736e03351d1a7c1fe4779
RonMankivsky <87318489+RonMankivsky@users.noreply.github.com>
2021-07-18 10:08:41 +0300

    Add files via upload

---
+++ onelineconvetor.ps1
@@ -0,0 +1,5 @@
+﻿$s = Get-Content in.txt
+$base64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($s))
+#powershell.exe -encodedCommand $base64
+$str = "POwerSheLl -wiNDOW HIddeN -encodedCommand "+$base64
+$str | Out-File out.txt
